0ea8233cfefa646e5b790835a6be60331c21a432
moonstar-x <christian.lopez99@outlook.com>
2023-05-21 02:21:52 -0500

    Created a temporary peptide controller.

--- api/controllers/__init__.py
+++ api/controllers/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from .health import health_controller
+from .peptides import peptides_controller
 
 api = Blueprint('api', __name__)
 
 api.register_blueprint(health_controller)
+api.register_blueprint(peptides_controller)
