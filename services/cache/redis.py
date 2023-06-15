fbf65811970367b7a3ad3b38a1d3e7db9f9039d0
moonstar-x <christian.lopez99@outlook.com>
2023-06-15 11:20:11 -0500

    Added prefix to redis keys for search redis client service.

--- services/cache/redis.py
+++ services/cache/redis.py
@@ -13,15 +13,21 @@ class RedisClientService:
 
 
 class SearchRedisClientService:
+    KEY_PREFIX = 'search'
+
     def __init__(self, service: RedisClientService):
         self.service = service
 
     def create_task(self, task_id: str, task_data: Any) -> None:
-        self.service.redis.set(task_id, json.dumps(task_data), ex=REDIS_TTL)
+        self.service.redis.set(SearchRedisClientService.resolve_key(task_id), json.dumps(task_data), ex=REDIS_TTL)
 
     def update_task(self, task_id: str, task_data: Any) -> None:
-        self.service.redis.set(task_id, json.dumps(task_data), keepttl=True, xx=True)
+        self.service.redis.set(SearchRedisClientService.resolve_key(task_id), json.dumps(task_data), keepttl=True, xx=True)
 
     def get_task(self, task_id: str) -> Any:
-        cached = self.service.redis.get(task_id)
+        cached = self.service.redis.get(SearchRedisClientService.resolve_key(task_id))
         return json.loads(cached) if cached is not None else None
+
+    @staticmethod
+    def resolve_key(task_id: str) -> str:
+        return f'{SearchRedisClientService.KEY_PREFIX}:{task_id}'
