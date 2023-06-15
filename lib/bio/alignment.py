2935a56438d1990e8c4cb4e16ad4c6f3015e95b5
moonstar-x <christian.lopez99@outlook.com>
2023-06-15 14:42:01 -0500

    Implemented align_multi_query function.

--- lib/bio/alignment.py
+++ lib/bio/alignment.py
@@ -1,18 +1,24 @@
 from __future__ import annotations
 from typing import Iterable, Optional, Dict, Any, List
 from dataclasses import dataclass
+from statistics import mean
 from Bio.Align import substitution_matrices, PairwiseAligner
 from services.database.models import Peptide
 
 
-# TODO: Implement multi alignment
 _SUPPORTED_MATRIX_NAMES = ('BLOSUM45', 'BLOSUM50', 'BLOSUM62', 'BLOSUM80', 'BLOSUM90', 'PAM30', 'PAM70', 'PAM250')
 _SUPPORTED_ALGORITHMS = ('global', 'local')
+_SUPPORTED_CRITERIA = ('avg', 'max', 'min')
 
 _DEFAULT_ALGORITHM = 'local'
 _DEFAULT_MATRIX_NAME = 'BLOSUM62'
 _DEFAULT_THRESHOLD = 1.0
 _DEFAULT_MAX_QUANTITY = None
+_DEFAULT_CRITERION = 'avg'
+
+
+def replace_ambiguous_amino_acids(seq: str) -> str:
+    return seq.replace('O', 'K').replace('J', 'L').replace('U', 'C')
 
 
 @dataclass
@@ -64,10 +70,6 @@ class SingleAlignmentOptions:
         return SingleAlignmentOptions(alg, matrix, threshold, max_quantity)
 
 
-def replace_ambiguous_amino_acids(seq: str) -> str:
-    return seq.replace('O', 'K').replace('J', 'L').replace('U', 'C')
-
-
 def align_single_query(database: Iterable[Peptide], query: str, options: SingleAlignmentOptions) -> List[SingleAlignedPeptide]:
     aligner = PairwiseAligner()
     aligner.substitution_matrix = substitution_matrices.load(options.matrix)
@@ -78,10 +80,77 @@ def align_single_query(database: Iterable[Peptide], query: str, options: SingleA
     result = []
     for target in database:
         score = aligner.score(replace_ambiguous_amino_acids(target.sequence), query)
-        score_ratio = score / float(max_score)
+        score_ratio = round(score / float(max_score), 2)
 
         if score_ratio >= options.threshold:
             result.append(SingleAlignedPeptide(target.id, target.sequence, target.length, score_ratio))
+
+    result.sort(key=lambda n: -n.score)
+
+    if options.max_quantity:
+        result = result[0:options.max_quantity]
+
+    return result
+
+
+@dataclass
+class MultiAlignedPeptide(Peptide):
+    score: float
+    avg_score: float
+    max_score: float
+    min_score: float
+
+
+@dataclass
+class MultiAlignmentOptions(SingleAlignmentOptions):
+    criterion: str
+
+    @staticmethod
+    def _validate_criterion(criterion: Optional[str]) -> None:
+        if criterion and criterion not in _SUPPORTED_CRITERIA:
+            raise ValueError(f'criterion must be one of: {", ".join(_SUPPORTED_CRITERIA)}')
+
+    @staticmethod
+    def create_from_params(params: Dict[str, Any]) -> MultiAlignmentOptions:
+        single_alignment_options = SingleAlignmentOptions.create_from_params(params)
+        criterion = params.get('criterion', _DEFAULT_CRITERION)
+
+        MultiAlignmentOptions._validate_criterion(criterion)
+
+        return MultiAlignmentOptions(**single_alignment_options.__dict__, criterion=criterion)
+
+
+def align_multi_query(database: Iterable[Peptide], queries: List[str], options: MultiAlignmentOptions) -> List[MultiAlignedPeptide]:
+    aligner = PairwiseAligner()
+    aligner.substitution_matrix = substitution_matrices.load(options.matrix)
+    aligner.mode = options.alg
+
+    result = []
+    for target in database:
+        scores_for_target = []
+
+        for query in queries:
+            max_score = sum([aligner.substitution_matrix[a, a] for a in query])
+            score = aligner.score(replace_ambiguous_amino_acids(target.sequence), query)
+            score_ratio = round(score / float(max_score), 2)
+
+            scores_for_target.append(score_ratio)
+
+        score_avg = round(mean(scores_for_target), 2)
+        score_max = round(max(scores_for_target), 2)
+        score_min = round(min(scores_for_target), 2)
+
+        if options.criterion == 'avg':
+            real_score = score_avg
+        elif options.criterion == 'max':
+            real_score = score_max
+        elif options.criterion == 'min':
+            real_score = score_min
+        else:
+            real_score = 0
+
+        if real_score >= options.threshold:
+            result.append(MultiAlignedPeptide(target.id, target.sequence, target.length, real_score, score_avg, score_max, score_min))
 
     result.sort(key=lambda n: -n.score)
 
