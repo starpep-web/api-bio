da96f8456d7da4b4156e7b33d2e90d0ac14b3e54
moonstar-x <christian.lopez99@outlook.com>
2023-08-02 21:36:25 -0500

    Updated PeptideController to get Peptide with attributes.

--- api/controllers/peptides/peptide_controller.py
+++ api/controllers/peptides/peptide_controller.py
@@ -9,7 +9,7 @@ peptide_controller = Blueprint('peptide', __name__, url_prefix='/')
 @peptide_controller.route('/', methods=['GET'])
 def get_peptides():
     peptides = db.peptides.get_all_peptides().as_mapped_object()
-    return ResponseBuilder().with_data(peptides).build()
+    return ResponseBuilder().with_data(list(peptides)).build()
 
 
 @peptide_controller.route('/<sequence>', methods=['GET'])
