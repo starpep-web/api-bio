58373b13c080db9b430a1b9ef1913bf97b411061
moonstar-x <christian.lopez99@outlook.com>
2023-08-19 20:31:20 -0500

    Created a util to transform base64 to bit array.

---
+++ lib/utils/export.py
@@ -0,0 +1,23 @@
+from typing import List
+from base64 import b64decode
+import binascii
+
+
+def base64to_bit_array(base64string: str) -> List[int]:
+    if not len(base64string):
+        raise ValueError('Cannot convert from empty base64 string.')
+
+    try:
+        decoded_bytes = b64decode(base64string, validate=True)
+    except binascii.Error:
+        raise ValueError('Invalid base64 string provided.')
+
+    bit_array = []
+    for byte in decoded_bytes:
+        binary = format(byte, '08b')
+        bit_array_for_byte = [1 if c == '1' else 0 for c in binary]
+
+        print(bit_array_for_byte)
+        bit_array += bit_array_for_byte
+
+    return bit_array
