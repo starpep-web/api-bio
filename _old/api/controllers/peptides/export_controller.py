ba309cbf23d516f5af715e247a33d0cf1d4c4866
moonstar-x <christian.lopez99@outlook.com>
2024-10-16 22:10:12 -0500

    Migrated post multi query export endpoint.

--- _old/api/controllers/peptides/export_controller.py
+++ _old/api/controllers/peptides/export_controller.py
@@ -19,29 +19,3 @@ def get_multi_query_export_task(task_id: str):
         raise ResourceNotFoundException(f'Multi query export task {task_id} does not exist.')
 
     return ResponseBuilder().with_data(cached_task).build()
-
-
-@export_controller.route('/multi-query', methods=['POST'])
-def post_multi_query_export_task():
-    try:
-        request_payload = SearchExportRequestPayload.from_json(request.json)
-    except Exception as e:
-        raise BadRequestException(str(e))
-
-    if not request_payload.is_multi_query():
-        raise BadRequestException('This endpoint only handles exporting multi-query searches.')
-
-    cached_search_task = MultiQueryAsyncTask.get_status(request_payload.data)
-    if cached_search_task is None:
-        raise ResourceNotFoundException(f'Multi query search task {request_payload.data} does not exist.')
-
-    if cached_search_task.loading:
-        raise ConflictException(f'Multi query search task {request_payload.data} has not finished yet.')
-
-    if not cached_search_task.success:
-        raise BadRequestException(f'Multi query search task {request_payload.data} was not successful.')
-
-    task = MultiQueryExportAsyncTask(request_payload, cached_search_task)
-    task.start()
-
-    return ResponseBuilder().with_data(task.get_init_status()).build()
--- pkg/shared/entity/export/multi_query/async_task.py
+++ pkg/shared/entity/export/multi_query/async_task.py
@@ -1,10 +1,10 @@
 import dataclasses
-from typing import Dict, Any, Optional, List
-from services.cache import cache
-from lib.bio.alignment import MultiAlignedPeptide
-from services.export.payload import SearchExportRequestPayload, SearchExportResult
-from services.export.archive import create_zip_archive
-from lib.asynchronous.task import AsyncTask, AsyncTaskStatus, S, E
+from typing import Optional, Dict, Any, List
+from pkg.shared.entity.export.redis import get_async_task_redis_client
+from pkg.shared.entity.export.models import SearchExportRequestPayload, SearchExportResult
+from pkg.shared.entity.export.utils import create_zip_archive
+from pkg.shared.entity.search.multi_query.model import MultiAlignedPeptide
+from pkg.shared.utils.async_task import AsyncTask, AsyncTaskStatus
 
 
 class MultiQueryExportAsyncTask(AsyncTask[Dict[str, Any], Exception]):
@@ -19,8 +19,9 @@ class MultiQueryExportAsyncTask(AsyncTask[Dict[str, Any], Exception]):
         self.result = None
 
     @staticmethod
-    def get_status(task_id: str) -> Optional[AsyncTaskStatus[S, E]]:
-        cached = cache.export.get_task(task_id)
+    def get_status(task_id: str) -> Optional[AsyncTaskStatus]:
+        cache = get_async_task_redis_client()
+        cached = cache.get_task(task_id)
 
         if cached is None or cached['name'] != MultiQueryExportAsyncTask.TASK_NAME:
             return None
@@ -28,14 +29,15 @@ class MultiQueryExportAsyncTask(AsyncTask[Dict[str, Any], Exception]):
         return AsyncTaskStatus(**cached)
 
     @staticmethod
-    def update_status(status: AsyncTaskStatus[S, E]) -> None:
-        cache.export.update_task(status.id, dataclasses.asdict(status))
+    def update_status(status: AsyncTaskStatus) -> None:
+        cache = get_async_task_redis_client()
+        cache.update_task(status.id, dataclasses.asdict(status))
 
     def handle_archive_progress(self, completed_resource: str) -> None:
         if self.result:
             self.result.done.append(completed_resource)
 
-            status = self.create_status(True, False, dataclasses.asdict(self.result))
+            status = self.create_status(True, False, self.result.to_dict())
             MultiQueryExportAsyncTask.update_status(status)
 
     def task(self) -> None:
@@ -48,12 +50,13 @@ class MultiQueryExportAsyncTask(AsyncTask[Dict[str, Any], Exception]):
         create_zip_archive(self.task_id, peptide_ids, self.payload.form, self.handle_archive_progress)
 
     def pre_run(self) -> None:
-        cache.export.create_task(self.task_id, dataclasses.asdict(self.get_init_status()))
+        cache = get_async_task_redis_client()
+        cache.create_task(self.task_id, dataclasses.asdict(self.get_init_status()))
 
         print(f'Started multi query export task {self.task_id}')
 
     def post_run(self) -> None:
-        status = self.create_status(False, True, dataclasses.asdict(self.result))
+        status = self.create_status(False, True, self.result.to_dict())
         MultiQueryExportAsyncTask.update_status(status)
 
         print(f'Finished multi query export task {self.task_id}')
